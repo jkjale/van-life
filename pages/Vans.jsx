d96d739d1659665fa23358283cca4a0de4828c55
Jake Lee <leejakejh@gmail.com>
2023-03-04 18:32:42 -0800

    add vans component

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 function Vans() {
+  React.useEffect(() => {
+    fetch('/api/vans')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, []);
+
   return (
     <h1>Vans page goes here</h1>
   );
